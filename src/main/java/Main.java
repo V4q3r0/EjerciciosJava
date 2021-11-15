46ead64baa2174f0fbea6a6c1ffcb4d2dc536608
V4q3r0 <alejandrocarvajal2018@gmail.com>
2021-11-15 18:52:33 -0500

    Se realizó el ejercicio 1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,14 @@
 public class Main {
+    public static void main(String[] args) {
+        int a = 10, b = 20, mayor = 0;
+        if(a == b){
+            System.out.println("Los números son iguales.");
+            System.exit(0);
+        }else if(a > b){
+            mayor = a;
+        }else{
+            mayor = b;
+        }
+        System.out.println("Entre los números "+a+" y "+b+" el mayor es: "+mayor);
+    }
 }
