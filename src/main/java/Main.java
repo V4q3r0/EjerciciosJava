3696a149d5754176bd2716dea26548750349d1ae
V4q3r0 <alejandrocarvajal2018@gmail.com>
2021-11-15 19:08:48 -0500

    Se realizó el ejercicio 2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,37 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int mayor = 0;
+        String a, b;
+        System.out.println("Escribe un número: ");
+        a = entrada.nextLine();
+        System.out.println("Escribe otro número para calcular: ");
+        b = entrada.nextLine();
+        if(isNumeric(a) && isNumeric(b)){
+            if(a.equals(b)){
+                System.out.println("Los números son iguales.");
+                System.exit(0);
+            }else if(Integer.parseInt(a) > Integer.parseInt(b)){
+                mayor = Integer.parseInt(a);
+            }else{
+                mayor = Integer.parseInt(b);
+            }
+        }else{
+            System.out.println("Los datos ingresados no son números, intenta de nuevo.");
+            System.exit(0);
+        }
+
+        System.out.println("Entre los números "+a+" y "+b+" el mayor es: "+mayor);
+    }
+
+    private static boolean isNumeric(String cadena){
+        try {
+            Integer.parseInt(cadena);
+            return true;
+        } catch (NumberFormatException nfe){
+            return false;
+        }
+    }
 }
