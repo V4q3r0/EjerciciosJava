9f9aa66c9d85beb3349e2f3934f2df0ff202f23a
V4q3r0 <alejandrocarvajal2018@gmail.com>
2021-11-15 20:01:40 -0500

    Se realizó el ejercicio 4

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        //Clase para recibir datos por teclado
+        Scanner entrada = new Scanner(System.in);
+        //variabes
+        final int IVA = 21;
+        String precio;
+        float total;
+        System.out.println("Escribe el precio del producto: ");
+        precio = entrada.nextLine();
+        if(isNumeric(precio)){
+            total = Float.parseFloat(precio) + (Float.parseFloat(precio)*IVA/100);
+            System.out.println("El precio total del producto es: "+total);
+        }else{
+            System.out.println("El dato ingresado no es númerico.");
+        }
+    }
+    //Funcion para comprobar si un dato es númerico.
+    private static boolean isNumeric(String cadena){
+        try{
+            Float.parseFloat(cadena);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+    }
 }
