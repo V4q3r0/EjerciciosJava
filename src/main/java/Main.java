b1ccf78f6947c40d22810b9350ed3316a4f8d32f
V4q3r0 <alejandrocarvajal2018@gmail.com>
2021-11-15 21:05:30 -0500

    Se realizó el ejercicio 9

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,10 @@
 public class Main {
+    public static void main(String[] args) {
+        //Variable
+        String cadena = "La sonrisa sera la mejor arma contra la tristeza", cadena2;
+        //Reemplazamos las a del String por e
+        cadena2 = cadena.replace('a', 'e');
+        //Mostramos por consola el string con sus cambios y concatenamos una frase.
+        System.out.println(cadena2+", no te compliques la vida, disfruta, no te quejes, aprende, no te enojes, sonrie.");
+    }
 }
