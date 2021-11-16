25246afe7e50c4fd0093c97c896edf326bed88c0
V4q3r0 <alejandrocarvajal2018@gmail.com>
2021-11-15 20:53:00 -0500

    Se realizó el ejercicio 8

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,38 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        //Objeto para recibir datos por teclado
+        Scanner entrada = new Scanner(System.in);
+        String dia;
+        //Se pide el dato por teclado, no se permite tíldes
+        System.out.println("Escribe un dia de la semana: ");
+        dia = entrada.nextLine();
+        switch (dia.toLowerCase()){
+            case "lunes":
+                System.out.println(dia+" es un día laboral!!");
+                break;
+            case "martes":
+                System.out.println(dia+" es un día laboral!!");
+                break;
+            case "miercoles":
+                System.out.println(dia+" es un día laboral!!");
+                break;
+            case "jueves":
+                System.out.println(dia+" es un día laboral!!");
+                break;
+            case "viernes":
+                System.out.println(dia+" es un día laboral!!");
+                break;
+            case "sabado":
+                System.out.println(dia+" NO es un día laboral!!");
+                break;
+            case "domingo":
+                System.out.println(dia+" NO es un día laboral!!");
+                break;
+            default:
+                System.out.println("El dato ingresado no coincide con día semanal!!");
+        }
+    }
 }
