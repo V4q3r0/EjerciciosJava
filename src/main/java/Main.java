a510cd6d08fbd95b6d53a0bd022d0a60a85649d6
V4q3r0 <alejandrocarvajal2018@gmail.com>
2021-11-15 20:35:23 -0500

    Se realizó el ejercicio 7

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,30 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        //Objeto para recibir datos por teclado
+        Scanner entrada = new Scanner(System.in);
+        //Variable
+        String num;
+        do{
+            System.out.println("Escribe un número: ");
+            num = entrada.nextLine();
+            if(isNumeric(num)){
+                if(Float.parseFloat(num) >= 0){
+                    System.out.println("El número es: "+num);
+                }
+            }else{
+                System.out.println("El dato ingresado no es númerico.");
+            }
+        }while(Float.parseFloat(num) < 0);
+    }
+    //Función para saber si el dato es númerico
+    private static boolean isNumeric(String cadena){
+        try{
+            Float.parseFloat(cadena);
+            return true;
+        }catch(Exception e){
+            return false;
+        }
+    }
 }
