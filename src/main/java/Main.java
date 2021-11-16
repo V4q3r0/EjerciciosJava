32e9ce3f8e9a8ebaa2298d9d03049cac50a6c262
V4q3r0 <alejandrocarvajal2018@gmail.com>
2021-11-15 19:26:45 -0500

    Se realizó el ejercicio 3

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        final float PI = (float) 3.1416;
+        float area = 0;
+        String radio;
+        System.out.println("Escribe el radio del circulo para calcular su area: ");
+        radio = entrada.nextLine();
+        if(isNumeric(radio)){
+            if(Float.parseFloat(radio) > 0){
+                area = (float) (PI*Math.pow(Double.parseDouble(radio),2));
+                System.out.println("El area del circulo es: "+area);
+            }else{
+                System.out.println("El radio debe ser un número positivo y mayor que 0.");
+                System.exit(0);
+            }
+        }else{
+            System.out.println("El dato ingresado no es númerico.");
+            System.exit(0);
+        }
+    }
+    private static boolean isNumeric(String cadena){
+        try{
+            Double.parseDouble(cadena);
+            return true;
+        }catch (Exception e){
+            System.out.println(e);
+            return false;
+        }
+    }
 }
