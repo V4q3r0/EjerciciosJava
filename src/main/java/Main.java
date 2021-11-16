fed0b680d312d559fba54f4d4883085a400e63c9
V4q3r0 <alejandrocarvajal2018@gmail.com>
2021-11-15 20:17:32 -0500

    Se realizó el ejercicio 5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,25 @@
 public class Main {
+    public static void main(String[] args) {
+        //variables
+        int i = 1, numero;
+        //números pares
+        System.out.println("NÚMEROS PARES!!");
+        while(i<=100){
+            numero = i % 2;
+            if(numero == 0){
+                System.out.println(i);
+            }
+            i++;
+        }
+        //Números impares
+        System.out.println("NÚMEROS IMPARES!!");
+        i = 1;
+        while(i<=100){
+            numero = i % 2;
+            if(numero != 0){
+                System.out.println(i);
+            }
+            i++;
+        }
+    }
 }
