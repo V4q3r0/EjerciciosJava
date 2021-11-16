01ee83280286f819216d45a3d6358fee2776be35
V4q3r0 <alejandrocarvajal2018@gmail.com>
2021-11-15 21:11:51 -0500

    Se realizó el ejercicio 10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        //Objeto para recibir datos por teclado
+        Scanner entrada = new Scanner(System.in);
+        //Variable
+        String cadena;
+        System.out.println("Escribe una frase: ");
+        cadena = entrada.nextLine();
+        System.out.println(cadena.replace(" ", ""));
+    }
 }
