f5d2616213a9c8fd1f258cd51d4ce471ed6ae2cc
V4q3r0 <alejandrocarvajal2018@gmail.com>
2021-11-17 21:43:43 -0500

    Se realiza el ejercicio 13

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,13 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
+    public static void main(String[] args) {
+        //Utilizamos la clase para crear el formato para la fecha y hora (como la queremos mostrar)
+        //yyyy = años, mm = mes, dd = dia, hh = hora, mm = minutos, ss = segundos
+        DateTimeFormatter time = DateTimeFormatter.ofPattern("yyyy/mm/dd hh/mm/ss");
+
+        //Mostramos la fecha y hora actual (now) usando el formato
+        System.out.println("La fecha y hora actual es: "+time.format(LocalDateTime.now()));
+    }
 }
