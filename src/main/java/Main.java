e143a829a19f81aa384f19442850a3663f115d42
V4q3r0 <alejandrocarvajal2018@gmail.com>
2021-11-17 21:50:46 -0500

    Se realiza el ejercicio 14

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        //Objeto para recibir datos por teclado
+        Scanner entrada = new Scanner(System.in);
+        //Variables
+        String number;
+
+        System.out.println("Escribe un número: ");
+        number = entrada.nextLine();
+
+        if(isNumeric(number) && Integer.parseInt(number) > 0){
+            for(int i = Integer.parseInt(number); i <= 1000; i += 2){
+                System.out.println("Número: "+i);
+            }
+        }else{
+            System.out.println("El dato indicado no es valido\nIngresa un número entero mayor que 0.");
+        }
+    }
+
+    //Función para saber si el dato ingresado es un número entero
+    private static Boolean isNumeric(String cadena){
+        try{
+            Integer.parseInt(cadena);
+            return true;
+        }catch (Exception e){
+            return false;
+        }
+
+    }
 }
